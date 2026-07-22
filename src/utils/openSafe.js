76f55b2c5aafd43707c1d5e1b2ef52d539dee3a8
https-Santiago <santiiifer03@gmail.com>
2026-07-22 11:08:08 -0300

    Disparar evento Lead de Meta en clics de WhatsApp

--- src/utils/openSafe.js
+++ src/utils/openSafe.js
@@ -1,4 +1,7 @@
 export const openSafe = (url) => {
+  if (/wa\.me|api\.whatsapp\.com/.test(url) && typeof window !== 'undefined' && window.fbq) {
+    window.fbq('track', 'Lead')
+  }
   const a = document.createElement('a')
   a.href = url
   a.target = '_blank'
